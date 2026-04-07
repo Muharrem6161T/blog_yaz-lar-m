5624e7e830cf03df3d2be39d55dc17fb51063591
Muharrem Talha Küçük <103388821+Muharrem6161T@users.noreply.github.com>
2026-04-07 16:29:26 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,18 @@
+API_URL = f"https://{SERVER_IP}/openapi/authorize/login"
+DATA  = {
+    "username" : "admin",
+    "password" : "Ka29101923."
+}
+PARAMS = {
+    "client_id" : "1d5b36240f024978a7bd60934503f95d",
+    "omadac_id" : OMADA_ID
+}
 
+HEADERS = {
+    "content-type" : "application/json"
+}
+result = requests.post(url=API_URL, json=DATA, params=PARAMS, headers=HEADERS, verify=False)
+    
+print(result.json()["result"])
+
+#>> {'csrfToken': 'lakjosnfdfoasndoıasndıoasdn', 'sessionId': 'iam-asmkdaslkmdlasmdasdkolllmkads'}
